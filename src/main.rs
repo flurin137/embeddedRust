3f6d83ff72d005d9d73414c22e44c817736abaad
Fx137 <flufeu@gmail.com>
2020-07-22 20:41:59 +0200

    added button functionality

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,25 @@ use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
 fn main() -> ! {
     let digital_out = pac::Peripherals::take().unwrap();
 
-    // Configure the clock.
     let mut clock = digital_out.RCC.freeze(Config::hsi16());
 
-    // Acquire the GPIOA peripheral. This also enables the clock for GPIOA in
-    // the RCC register.
     let gpioa = digital_out.GPIOA.split(&mut clock);
+    let gpioc = digital_out.GPIOC.split(&mut clock);
 
-    // Configure PA1 as output.
     let mut led = gpioa.pa5.into_push_pull_output();
+    let button = gpioc.pc13.into_pull_up_input();
 
     loop {
-        for _ in 0..1_000_000 {
+        let limit = match button.is_high() {
+            Ok(true) => 1_000_000,
+            Ok(false) => 300_000,
+            _ => unreachable!(),
+        };
+
+        for _ in 0..limit {
             led.set_high().unwrap();
         }
-        for _ in 0..1_000_000 {
+        for _ in 0..limit {
             led.set_low().unwrap();
         }
     }
