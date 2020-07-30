31a23f5e26ed78c40555580dc3ca9b8fcaa597e4
Fx137 <flufeu@gmail.com>
2020-07-30 21:14:03 +0200

    fancyer blinking

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 #![no_std]
 
 use panic_halt as _;
-use cortex_m_rt::entry;
 
+use cortex_m_rt::entry;
 use stm32l0xx_hal::{pac, prelude::*, pwm, rcc::Config};
 
 #[entry]
@@ -19,7 +19,7 @@ fn main() -> ! {
     let gpiob = board_peripherals.GPIOB.split(&mut clock);
     let gpioc = board_peripherals.GPIOC.split(&mut clock);
 
-    let button = gpioc.pc13.into_pull_up_input();
+    let button = gpioa.pa0.into_pull_up_input();
 
     // Initialize TIM2 for PWM
     let timer2 = pwm::Timer::new(board_peripherals.TIM2, 10.khz(), &mut clock);
@@ -27,29 +27,27 @@ fn main() -> ! {
     let mut pwm1 = timer2.channel2.assign(gpiob.pb3);
     let mut pwm2 = timer2.channel3.assign(gpiob.pb10);
 
-    loop {
-        let limit : u16 = match button.is_high() {
-            Ok(true) => 4,
-            Ok(false) => 1,
-            _ => 1,
-        };
-        
-        let max1 = pwm1.get_max_duty() / limit;
+    let max1 = pwm1.get_max_duty();
 
-        pwm1.enable();
-        pwm2.enable();
-        
-        loop{
-            for i in 0..max1 {
-                pwm1.set_duty(i);
-                pwm2.set_duty(max1-i);
-                delay.delay_ms(5_u16);
-            }
-            for i in 0..max1 {
-                pwm2.set_duty(i);
-                pwm1.set_duty(max1-i);
-                delay.delay_ms(5_u16);
-            }
+    pwm1.enable();
+    pwm2.enable();
+    
+    loop{
+        let limit = match button.is_high() {
+            Ok(true) => 1.ms(),
+            Ok(false) => 5.ms(),
+            _ => unreachable!(),
+        };
+
+        for i in 0..max1 {
+            pwm1.set_duty(i);
+            pwm2.set_duty(max1-i);
+            delay.delay(limit);
+        }
+        for i in 0..max1 {
+            pwm2.set_duty(i);
+            pwm1.set_duty(max1-i);
+            delay.delay(limit);
         }
     }
 }
